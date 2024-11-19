25fc844e7a9a54a9ff92084851d4899cbb8e90f2
GalloSimone <gallosimonecz@gmail.com>
2024-11-19 16:08:26 +0100

    creazione cartella public/images

--- app.js
+++ app.js
@@ -5,8 +5,49 @@ const port = 3000
 app.get('/', (req, res) => {
     res.send('Server del mio blog!')
     });
+    app.get('/bacheca',(req, res)=> {
+        const posts =[
+            {   
+                titolo: "Ciambellone",
+                contenuto:"Un dolce soffice e genuino, perfetto per addolcire le tue giornate con un tocco di tradizione casalinga.",
+                img:"./public/img/ciambellone.jpeg",
+                tags : ["ciambellone", "tradizione"]
+            },
+            {   
+                titolo: "Cracker alla barbabietola",
+                contenuto:"Leggeri e croccanti, questi cracker uniscono il sapore unico della barbabietola a una bontà irresistibile.",
+                img:"./public/img/cracker_barbabietola.jpeg",
+                tags : ["cracker","salato","barbabietola"]
+            },
+            {   
+                titolo: "Pane fritto",
+                contenuto:" Una delizia dorata e fragrante, ricoperta di zucchero o miele, che regala un'esplosione di dolcezza al primo morso.",
+                img:"./public/img/cracker_barbabietola.jpeg",
+                tags : ["pane", "fritto"]
+            },
+            {   
+                titolo: "Pasta alla barbabietola",
+                contenuto:"Un primo piatto scenografico e raffinato, dal sapore delicato e dalla crema vellutata che conquista",
+                img:"./public/img/pasta_barbabietola.jpeg",
+                tags : ["pasta", "barbabietola"]
+            },
+            {   
+                titolo: "Torta paesana",
+                contenuto:"Il dolce rustico che racchiude tutta la magia dei sapori antichi, con note di cacao, uvetta e pinoli.",
+                img:"./public/img/torta_paesana.jpeg",
+                tags : ["dolce", "torta", "paesana"]
+            }]
+
+            res.json({
+                number : posts.length,
+                posts :posts
+            })
+            })
+
+
+
 
 
     app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`il server risulta online`)
     })
