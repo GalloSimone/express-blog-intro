3f470d7f85a8a2eb05016939ba353e284081767d
GalloSimone <gallosimonecz@gmail.com>
2024-11-19 16:41:35 +0100

    add modify

--- app.js
+++ app.js
@@ -12,31 +12,31 @@ app.get('/', (req, res) => {
             {   
                 titolo: "Ciambellone",
                 contenuto:"Un dolce soffice e genuino, perfetto per addolcire le tue giornate con un tocco di tradizione casalinga.",
-                img:"./public/img/ciambellone.jpeg",
+                img:"./public/images/ciambellone.jpeg",
                 tags : ["ciambellone", "tradizione"]
             },
             {   
                 titolo: "Cracker alla barbabietola",
                 contenuto:"Leggeri e croccanti, questi cracker uniscono il sapore unico della barbabietola a una bontà irresistibile.",
-                img:"./public/img/cracker_barbabietola.jpeg",
-                tags : ["cracker","salato","barbabietola"]
+                img:"./public/images/cracker_barbabietola.jpeg",
+                tags : ["cracker","barbabietola"]
             },
             {   
                 titolo: "Pane fritto",
                 contenuto:" Una delizia dorata e fragrante, ricoperta di zucchero o miele, che regala un'esplosione di dolcezza al primo morso.",
-                img:"./public/img/cracker_barbabietola.jpeg",
+                img:"./public/images/cracker_barbabietola.jpeg",
                 tags : ["pane", "fritto"]
             },
             {   
                 titolo: "Pasta alla barbabietola",
                 contenuto:"Un primo piatto scenografico e raffinato, dal sapore delicato e dalla crema vellutata che conquista",
-                img:"./public/img/pasta_barbabietola.jpeg",
+                img:"./public/images/pasta_barbabietola.jpeg",
                 tags : ["pasta", "barbabietola"]
             },
             {   
                 titolo: "Torta paesana",
                 contenuto:"Il dolce rustico che racchiude tutta la magia dei sapori antichi, con note di cacao, uvetta e pinoli.",
-                img:"./public/img/torta_paesana.jpeg",
+                img:"./public/images/torta_paesana.jpeg",
                 tags : ["dolce", "torta", "paesana"]
             }]
 
